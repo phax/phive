4f8b357ec1bed2e34506bb9613332359eb627841
Philip Helger <philip@helger.com>
2014-12-01 21:59:29 +0100

    Linked transaction to UBL document type

--- src/main/java/com/helger/peppol/validation/domain/ETransaction.java
+++ src/main/java/com/helger/peppol/validation/domain/ETransaction.java
@@ -25,6 +25,7 @@ import com.helger.commons.id.IHasID;
 import com.helger.commons.lang.EnumHelper;
 import com.helger.commons.name.IHasName;
 import com.helger.commons.string.StringHelper;
+import com.helger.ubl.EUBL21DocumentType;
 
 /**
  * Represents a single CEN BII2 transaction used in several profiles.<br>
@@ -41,40 +42,40 @@ import com.helger.commons.string.StringHelper;
  */
 public enum ETransaction implements IHasID <String>, IHasName
 {
-  T01 ("Order", 1, null, "2.0", EGroup.POST_AWARD),
-  T02 ("Simple Order Response", 2, null, "2.0", EGroup.POST_AWARD),
-  T04 ("Counter Offer", 4, null, "2.0", EGroup.POST_AWARD),
-  T05 ("Simple Counter Offer Response", 5, null, "2.0", EGroup.POST_AWARD),
-  T10 ("Invoice", 10, null, "2.0", EGroup.POST_AWARD),
-  T14 ("Credit Note", 14, null, "2.0", EGroup.POST_AWARD),
-  T16 ("Dispatch Advice", 16, null, "2.0", EGroup.POST_AWARD),
-  T17 ("Reminder", 17, null, "2.0", EGroup.POST_AWARD),
-  T18 ("Catalogue Request", 18, null, "2.0", EGroup.CATALOGUE),
-  T19 ("Catalogue", 19, null, "2.0", EGroup.CATALOGUE),
-  T20 ("Catalogue Item Update", 20, null, "2.0", EGroup.CATALOGUE),
-  T21 ("Catalogue Price Update", 21, null, "2.0", EGroup.CATALOGUE),
-  T22 ("Catalogue Delete Request", 22, null, "2.0", EGroup.CATALOGUE),
-  T23 ("Catalogue Delete Confirmation", 23, null, "2.0", EGroup.CATALOGUE),
-  T26 ("Statement", 26, null, "2.0", EGroup.POST_AWARD),
-  T40 ("Call for Tender", 40, null, "1.0", EGroup.TENDERING),
-  T41 ("Qualification", 41, null, "1.0", EGroup.TENDERING),
-  T42 ("Qualification Reception Notification", 42, null, "1.0", EGroup.TENDERING),
-  T44 ("Tender", 44, null, "1.0", EGroup.TENDERING),
-  T45 ("Tender Reception Notification", 45, null, "1.0", EGroup.TENDERING),
-  T54 ("Multi Party Catalogue", 54, null, "2.0", EGroup.CATALOGUE),
-  T55 ("Catalogue Request Rejection", 55, null, "2.0", EGroup.CATALOGUE),
-  T58 ("Catalogue Response", 58, null, "2.0", EGroup.CATALOGUE),
-  T59 ("Catalogue Update Response", 59, null, "2.0", EGroup.CATALOGUE),
-  T64A ("Prior Information Notice", 64, "A", "1.0", EGroup.NOTIFICATION),
-  T64B ("Contract  Notice", 64, "B", "1.0", EGroup.NOTIFICATION),
-  T64C ("Contract Award Notice", 64, "C", "1.0", EGroup.NOTIFICATION),
-  T65 ("Notice Publication Response", 65, null, "1.0", EGroup.NOTIFICATION),
-  T68 ("Pre-award Catalogue", 68, null, "1.0", EGroup.TENDERING),
-  T69 ("Pre-award Catalogue Template", 69, null, "1.0", EGroup.TENDERING),
-  T71 ("Message Level Response", 71, null, "1.0", EGroup.ARCHITECTURE),
-  T72 ("Catalogue Subscription Request", 72, null, "1.0", EGroup.CATALOGUE),
-  T73 ("Catalogue Subscription Response", 73, null, "1.0", EGroup.CATALOGUE),
-  T76 ("Order Response", 76, null, "1.0", EGroup.POST_AWARD);
+  T01 ("Order", 1, null, "2.0", EGroup.POST_AWARD, EUBL21DocumentType.ORDER),
+  T02 ("Simple Order Response", 2, null, "2.0", EGroup.POST_AWARD, EUBL21DocumentType.ORDER_RESPONSE_SIMPLE),
+  T04 ("Counter Offer", 4, null, "2.0", EGroup.POST_AWARD, EUBL21DocumentType.ORDER_RESPONSE),
+  T05 ("Simple Counter Offer Response", 5, null, "2.0", EGroup.POST_AWARD, EUBL21DocumentType.ORDER_CHANGE),
+  T10 ("Invoice", 10, null, "2.0", EGroup.POST_AWARD, EUBL21DocumentType.INVOICE),
+  T14 ("Credit Note", 14, null, "2.0", EGroup.POST_AWARD, EUBL21DocumentType.CREDIT_NOTE),
+  T16 ("Dispatch Advice", 16, null, "2.0", EGroup.POST_AWARD, EUBL21DocumentType.DESPATCH_ADVICE),
+  T17 ("Reminder", 17, null, "2.0", EGroup.POST_AWARD, EUBL21DocumentType.REMINDER),
+  T18 ("Catalogue Request", 18, null, "2.0", EGroup.CATALOGUE, EUBL21DocumentType.CATALOGUE_REQUEST),
+  T19 ("Catalogue", 19, null, "2.0", EGroup.CATALOGUE, EUBL21DocumentType.CATALOGUE),
+  T20 ("Catalogue Item Update", 20, null, "2.0", EGroup.CATALOGUE, EUBL21DocumentType.CATALOGUE_ITEM_SPECIFICATION_UPDATE),
+  T21 ("Catalogue Price Update", 21, null, "2.0", EGroup.CATALOGUE, EUBL21DocumentType.CATALOGUE_PRICING_UPDATE),
+  T22 ("Catalogue Delete Request", 22, null, "2.0", EGroup.CATALOGUE, EUBL21DocumentType.CATALOGUE_DELETION),
+  T23 ("Catalogue Delete Confirmation", 23, null, "2.0", EGroup.CATALOGUE, EUBL21DocumentType.APPLICATION_RESPONSE),
+  T26 ("Statement", 26, null, "2.0", EGroup.POST_AWARD, EUBL21DocumentType.STATEMENT),
+  T40 ("Call for Tender", 40, null, "1.0", EGroup.TENDERING, EUBL21DocumentType.CALL_FOR_TENDERS),
+  T41 ("Qualification", 41, null, "1.0", EGroup.TENDERING, EUBL21DocumentType.TENDER_QUALIFICATION),
+  T42 ("Qualification Reception Notification", 42, null, "1.0", EGroup.TENDERING, EUBL21DocumentType.TENDER_QUALIFICATION_RESPONSE),
+  T44 ("Tender", 44, null, "1.0", EGroup.TENDERING, EUBL21DocumentType.TENDER),
+  T45 ("Tender Reception Notification", 45, null, "1.0", EGroup.TENDERING, EUBL21DocumentType.TENDER_RECEIPT),
+  T54 ("Multi Party Catalogue", 54, null, "2.0", EGroup.CATALOGUE, EUBL21DocumentType.CATALOGUE),
+  T55 ("Catalogue Request Rejection", 55, null, "2.0", EGroup.CATALOGUE, EUBL21DocumentType.APPLICATION_RESPONSE),
+  T58 ("Catalogue Response", 58, null, "2.0", EGroup.CATALOGUE, EUBL21DocumentType.APPLICATION_RESPONSE),
+  T59 ("Catalogue Update Response", 59, null, "2.0", EGroup.CATALOGUE, EUBL21DocumentType.APPLICATION_RESPONSE),
+  T64A ("Prior Information Notice", 64, "A", "1.0", EGroup.NOTIFICATION, EUBL21DocumentType.PRIOR_INFORMATION_NOTICE),
+  T64B ("Contract  Notice", 64, "B", "1.0", EGroup.NOTIFICATION, EUBL21DocumentType.CONTRACT_NOTICE),
+  T64C ("Contract Award Notice", 64, "C", "1.0", EGroup.NOTIFICATION, EUBL21DocumentType.CONTRACT_AWARD_NOTICE),
+  T65 ("Notice Publication Response", 65, null, "1.0", EGroup.NOTIFICATION, EUBL21DocumentType.APPLICATION_RESPONSE),
+  T68 ("Pre-award Catalogue", 68, null, "1.0", EGroup.TENDERING, EUBL21DocumentType.CATALOGUE),
+  T69 ("Pre-award Catalogue Template", 69, null, "1.0", EGroup.TENDERING, EUBL21DocumentType.CATALOGUE),
+  T71 ("Message Level Response", 71, null, "1.0", EGroup.ARCHITECTURE, EUBL21DocumentType.APPLICATION_RESPONSE),
+  T72 ("Catalogue Subscription Request", 72, null, "1.0", EGroup.CATALOGUE, EUBL21DocumentType.CATALOGUE_REQUEST),
+  T73 ("Catalogue Subscription Response", 73, null, "1.0", EGroup.CATALOGUE, EUBL21DocumentType.APPLICATION_RESPONSE),
+  T76 ("Order Response", 76, null, "1.0", EGroup.POST_AWARD, EUBL21DocumentType.ORDER_RESPONSE);
 
   private final String m_sID;
   private final String m_sName;
@@ -83,12 +84,14 @@ public enum ETransaction implements IHasID <String>, IHasName
   private final String m_sVersion;
   private final String m_sTransactionID;
   private final EGroup m_eGroup;
+  private final EUBL21DocumentType m_eUBLDocumentType;
 
   private ETransaction (@Nonnull @Nonempty final String sName,
                         @Nonnegative final int nNumber,
                         @Nullable final String sSubNumber,
                         @Nonnull @Nonempty final String sVersion,
-                        @Nonnull final EGroup eGroup)
+                        @Nonnull final EGroup eGroup,
+                        @Nonnull final EUBL21DocumentType eUBLDocumentType)
   {
     m_sID = name ();
     m_sName = sName;
@@ -101,6 +104,7 @@ public enum ETransaction implements IHasID <String>, IHasName
                        ":ver" +
                        sVersion;
     m_eGroup = eGroup;
+    m_eUBLDocumentType = eUBLDocumentType;
   }
 
   @Nonnull
@@ -163,13 +167,22 @@ public enum ETransaction implements IHasID <String>, IHasName
   }
 
   /**
-   * @return The version of the transaction. Either "1.0" or "2.0".
+   * @return The version of the transaction. E.g. "1.0" or "2.0".
    */
   @Nonnull
   @Nonempty
   public String getVersionNumber ()
   {
     return m_sVersion;
+  }
+
+  /**
+   * @return The expected UBL 2.1 document type. Never <code>null</code>.
+   */
+  @Nonnull
+  public final EUBL21DocumentType getUBLDocumentType ()
+  {
+    return m_eUBLDocumentType;
   }
 
   @Nullable
