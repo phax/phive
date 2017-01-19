d4d308bec6603e7acdf844be52cd6fea316b3f22
Philip Helger <philip@helger.com>
2017-01-19 12:32:02 +0100

    Updated license headers

--- peppol-validation-engine/src/main/java/com/helger/peppol/validation/engine/peppol/IPeppolValidationArtefact.java
+++ peppol-validation-engine/src/main/java/com/helger/peppol/validation/engine/peppol/IPeppolValidationArtefact.java
@@ -1,8 +1,30 @@
+/**
+ * Copyright (C) 2014-2017 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.helger.peppol.validation.engine.peppol;
 
 import com.helger.bdve.artefact.IValidationArtefact;
 
-public interface IPeppolValidationArtefact extends IValidationArtefact
+/**
+ * Dummy interface that only defines the internal paths for the PEPPOL
+ * validation artefacts
+ * 
+ * @author Philip Helger
+ */
+interface IPeppolValidationArtefact extends IValidationArtefact
 {
   // global prefix
   String PREFIX = "openpeppol/3.3.0/SCH/";
