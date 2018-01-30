5866d1108be1bac6819c0e1df0e62bc562915728
Philip Helger <philip@helger.com>
2018-01-30 15:12:08 +0100

    Improved log message

--- ph-bdve-en16931/src/test/java/com/helger/bdve/en16931/ValidationExecutionManagerFuncTest.java
+++ ph-bdve-en16931/src/test/java/com/helger/bdve/en16931/ValidationExecutionManagerFuncTest.java
@@ -70,7 +70,7 @@ public final class ValidationExecutionManagerFuncTest
       final ValidationResultList aErrors = aValidator.executeValidation (aSource, Locale.US);
       if (aTestFile.isGoodCase ())
         assertTrue (aErrors.getAllCount (IError::isError) +
-                    " error:\n" +
+                    " error(s):\n" +
                     StringHelper.getImploded ('\n', aErrors.getAllErrors ()),
                     aErrors.containsNoError ());
       else
