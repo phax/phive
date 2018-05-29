1e2a720de5fe28e2d469debf05e350cb0498a5b7
Philip Helger <philip@helger.com>
2018-05-29 21:35:48 +0200

    Started TEAPPS

---
+++ ph-bdve-teapps/src/main/java/com/helger/bdve/teapps/TEAPPSValidation.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (C) 2014-2018 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.bdve.teapps;
+
+import javax.annotation.Nonnull;
+import javax.annotation.concurrent.Immutable;
+
+import com.helger.bdve.executorset.VESID;
+import com.helger.bdve.executorset.ValidationExecutorSet;
+import com.helger.bdve.executorset.ValidationExecutorSetRegistry;
+import com.helger.bdve.key.ValidationArtefactKey;
+import com.helger.bdve.spec.BusinessSpecification;
+import com.helger.bdve.spec.BusinessSpecificationRegistry;
+import com.helger.bdve.spec.IBusinessSpecification;
+import com.helger.bdve.spec.SpecificationTransaction;
+import com.helger.commons.ValueEnforcer;
+import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.collection.impl.CommonsArrayList;
+import com.helger.jaxb.builder.JAXBDocumentType;
+
+/**
+ * Generic TEAPPS validation configuration
+ *
+ * @author Philip Helger
+ */
+@Immutable
+public final class TEAPPSValidation
+{
+  @Nonnull
+  private static BusinessSpecification _createBusinessSpec (@Nonnull @Nonempty final String sGroupID,
+                                                            @Nonnull @Nonempty final String sSpecID,
+                                                            @Nonnull @Nonempty final String sDisplayName)
+  {
+    final BusinessSpecification ret = new BusinessSpecification (sGroupID, sSpecID, sDisplayName);
+    BusinessSpecificationRegistry.INSTANCE.registerBusinessSpecification (ret);
+    return ret;
+  }
+
+  public static final String GROUP_ID = "com.tieto";
+  public static final String VERSION_272 = "2.7.2";
+  public static final String VERSION_30 = "3.0";
+  public static final IBusinessSpecification TEAPPS = _createBusinessSpec (GROUP_ID, "TEAPPS", "TEAPPSXML");
+
+  public static final VESID VID_TEAPPS_272 = new VESID (GROUP_ID, "TEAPPSXML", VERSION_272);
+  public static final VESID VID_TEAPPS_30 = new VESID (GROUP_ID, "TEAPPSXML", VERSION_30);
+
+  private TEAPPSValidation ()
+  {}
+
+  /**
+   * Register all standard TEAPPS D16B validation execution sets to the provided
+   * registry.
+   *
+   * @param aRegistry
+   *        The registry to add the artefacts. May not be <code>null</code>.
+   */
+  public static void initTEAPPS (@Nonnull final ValidationExecutorSetRegistry aRegistry)
+  {
+    ValueEnforcer.notNull (aRegistry, "Registry");
+
+    // No Schematrons here
+    aRegistry.registerValidationExecutorSet (ValidationExecutorSet.create (VID_TEAPPS_272,
+                                                                           "TEAPPSXML " + VERSION_272,
+                                                                           new ValidationArtefactKey.Builder ().setBusinessSpecification (TEAPPS)
+                                                                                                               .setTransaction (new SpecificationTransaction ("teapps272",
+                                                                                                                                                              "TEAPPSXML 2.7.2",
+                                                                                                                                                              new JAXBDocumentType (String.class,
+                                                                                                                                                                                    new CommonsArrayList <> ("/schemas/TEAPPSXMLv272_schema_INVOICES.xsd"),
+                                                                                                                                                                                    null),
+                                                                                                                                                              null))
+                                                                                                               .build (),
+                                                                           false));
+  }
+}
