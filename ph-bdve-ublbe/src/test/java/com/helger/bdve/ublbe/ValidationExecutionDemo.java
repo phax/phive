e988bdd4a1c27762a9f14372454ea7f89a902395
Philip Helger <philip@helger.com>
2020-02-06 14:14:22 +0100

    Avoid warning

--- ph-bdve-ublbe/src/test/java/com/helger/bdve/ublbe/ValidationExecutionDemo.java
+++ ph-bdve-ublbe/src/test/java/com/helger/bdve/ublbe/ValidationExecutionDemo.java
@@ -41,7 +41,7 @@ public final class ValidationExecutionDemo
   public void testCode (@Nullable final String sSystemID, @Nonnull final byte [] aXML)
   {
     // Example: validate against orders
-    final VESID aVESID = UBLBEValidation.VID_EFFF_INVOICE;
+    final VESID aVESID = UBLBEValidation.VID_UBL_BE_INVOICE_110;
 
     // Note: Use the currently active version
     final IValidationExecutorSet aExecutors = CTestFiles.VES_REGISTRY.getOfID (aVESID);
