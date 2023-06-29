59dc9805882137d6a1837d497e78822db0d0e47e
Stefan Katzensteiner <stefan.katzensteiner@ecosio.com>
2023-06-29 12:54:10 +0200

    RepoStorageInMemoryTest

--- phive-engine/src/test/java/com/helger/phive/engine/repo/impl/RepoStorageInMemoryTest.java
+++ phive-engine/src/test/java/com/helger/phive/engine/repo/impl/RepoStorageInMemoryTest.java
@@ -5,6 +5,49 @@
  */
 package com.helger.phive.engine.repo.impl;
 
+import com.helger.commons.state.ESuccess;
+import com.helger.phive.engine.repo.EHashState;
+import com.helger.phive.engine.repo.RepoStorageItem;
+import com.helger.phive.engine.repo.RepoStorageKey;
+
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
 public final class RepoStorageInMemoryTest {
 
+  @Test
+  public void testWrite ()
+  {
+    final RepoStorageInMemory aRepo = new RepoStorageInMemory ();
+
+    final RepoStorageKey aKey = RepoStorageKey.of ("com/ecosio/written/inmemory");
+    // Ensure not existing
+    assertNull (aRepo.read (aKey));
+
+    try
+    {
+      // Write
+      final ESuccess eSuccess = aRepo.write (aKey, RepoStorageItem.of ("bla".getBytes (StandardCharsets.ISO_8859_1)));
+      assertTrue (eSuccess.isSuccess ());
+
+      // Read again
+      final RepoStorageItem aItem = aRepo.read (aKey);
+      assertNotNull (aItem);
+      assertEquals ("bla", aItem.getDataAsString (StandardCharsets.UTF_8));
+      assertSame (EHashState.VERIFIED_MATCHING, aItem.getHashState ());
+    }
+    finally
+    {
+      // Cleanup
+      aRepo.delete (aKey);
+      aRepo.delete (aKey.getKeyHashSha256 ());
+    }
+  }
 }
