f61472c2263d967ddd32da29b5cd19f3b7b9179d
Philip Helger <philip@helger.com>
2023-07-19 17:06:05 +0200

    Moved mock class

--- phive-api/src/main/java/com/helger/phive/api/mock/MockFile.java
+++ phive-api/src/main/java/com/helger/phive/api/mock/MockFile.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.helger.phive.engine.mock;
+package com.helger.phive.api.mock;
 
 import java.util.Set;
 
