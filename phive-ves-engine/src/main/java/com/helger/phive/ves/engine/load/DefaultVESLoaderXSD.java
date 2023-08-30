76e06bad7bcbe3f21e7f49b946739a046c795ae9
Philip Helger <philip@helger.com>
2023-08-30 18:56:01 +0200

    Updated license header

--- phive-ves-engine/src/main/java/com/helger/phive/ves/engine/load/DefaultVESLoaderXSD.java
+++ phive-ves-engine/src/main/java/com/helger/phive/ves/engine/load/DefaultVESLoaderXSD.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014-2023 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.helger.phive.ves.engine.load;
 
 import java.io.IOException;
@@ -212,15 +228,17 @@ public class DefaultVESLoaderXSD implements IVESLoaderXSD
             if (sMainUnified.equals (sEntryNameUnified))
               bFoundMain = true;
 
+            // Read ZIP entry
             try (final NonBlockingByteArrayOutputStream aBAOS = new NonBlockingByteArrayOutputStream ())
             {
-              // Read ZIP entry
               int nLen;
               while ((nLen = aZIS.read (aBuffer)) > 0)
               {
                 aBAOS.write (aBuffer, 0, nLen);
                 nUnzippedLed += nLen;
               }
+
+              // Remember ZIP entry in map
               aZIPContent.put (sEntryNameUnified, aBAOS);
             }
           }
@@ -288,6 +306,7 @@ public class DefaultVESLoaderXSD implements IVESLoaderXSD
                             "'");
 
             // Catalog has precedence
+            // TODO resolve by catalog
 
             // Search in ZIP
             final NonBlockingByteArrayOutputStream aZIPResolved = aZIPContent.get (sRelativeSystemId);
--- phive-ves-engine/src/main/java/com/helger/phive/ves/engine/load/VESLoader.java
+++ phive-ves-engine/src/main/java/com/helger/phive/ves/engine/load/VESLoader.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014-2023 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.helger.phive.ves.engine.load;
 
 import javax.annotation.Nonnull;
@@ -227,6 +243,7 @@ public final class VESLoader
   {
     ValueEnforcer.notNull (aStatus, "Status");
     ValueEnforcer.notNull (aVES, "VES");
+    ValueEnforcer.notNull (aLoaderStatus, "LoaderStatus");
     ValueEnforcer.notNull (aErrorList, "ErrorList");
 
     final EVESSyntax eSyntax = aVES.getXsd () != null ? EVESSyntax.XSD : aVES.getSchematron () != null
@@ -349,6 +366,11 @@ public final class VESLoader
                                   @Nonnull final VESLoaderStatus aLoaderStatus,
                                   @Nonnull final ErrorList aErrorList)
   {
+    ValueEnforcer.notNull (aStatus, "Status");
+    ValueEnforcer.notNull (aVESRes, "VESRes");
+    ValueEnforcer.notNull (aLoaderStatus, "LoaderStatus");
+    ValueEnforcer.notNull (aErrorList, "ErrorList");
+
     // Read VES as XML
     final VesType aVES = new VES1Marshaller ().setCollectErrors (aErrorList).read (aVESRes);
     if (aVES == null)
